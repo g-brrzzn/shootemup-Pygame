bf8fc281792d160dff5fa0fe0719f1d0f77ed8e8
Gabriel B <136928835+g-brrzzn@users.noreply.github.com>
2023-09-22 13:39:12 -0300

    Update imports

--- constants/global_imports.py
+++ constants/global_imports.py
@@ -1,8 +1,5 @@
-from random import randint, uniform, choices, choice
-from string import ascii_uppercase
+from random import randint, uniform, choice
 import sys
 import pygame
 from pygame.locals import *
-from PIL import Image
 from time import time
-from math import ceil
